f9603517a02b0d0bac06c8d4f20acdcadb80bbf8
Rob Smith <kormoc@gmail.com>
2012-06-30 00:26:15 -0700

    Hex dump packets on the wire

--- JMTC_Thread.java
+++ JMTC_Thread.java
@@ -1,54 +1,164 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import org.apache.commons.io.*;
 
 public class JMTC_Thread extends Thread {
     private Socket socket = null;
+    private Socket mysqlSocket = null;
     private String mysqlHost = null;
     private int mysqlPort;
+    
+    private InputStream clientIn = null;
+    private OutputStream clientOut = null;
+    
+    private InputStream mysqlIn = null;
+    private OutputStream mysqlOut = null;
+    
+    private ArrayList<Integer> buffer = new ArrayList<Integer>();
+    
+    private int running = 1;
 
     public JMTC_Thread(Socket socket, String mysqlHost, int mysqlPort) {
         this.socket = socket;
         this.mysqlHost = mysqlHost;
         this.mysqlPort = mysqlPort;
-    }
-
-    public void run() {
-        try {
-            InputStream clientIn = this.socket.getInputStream();
-            OutputStream clientOut = this.socket.getOutputStream();
         
-            Socket mysql = new Socket(this.mysqlHost, this.mysqlPort);
-            InputStream mysqlIn = mysql.getInputStream();
-            OutputStream mysqlOut = mysql.getOutputStream();
-            
-            while (true) {
-                ArrayList<Integer> buffer = new ArrayList<Integer>();
-                int b = 0;
-                
-                // Read from the client
-                buffer.clear();
-                while (clientIn.available() > 0) {
-                    b = clientIn.read();
-                    if (b == -1)
-                        break;
-                    buffer.add(b);
-                    mysqlOut.write(b);
-                }
-                
-                // Read from the server
-                buffer.clear();
-                while (mysqlIn.available() > 0) {
-                    b = mysqlIn.read();
-                    if (b == -1)
-                        break;
-                    buffer.add(b);
-                    clientOut.write(b);
-                }
-            }
+        try {
+            this.clientIn = this.socket.getInputStream();
+            this.clientOut = this.socket.getOutputStream();
+        
+            // Connect to the mysql server on the other side
+            this.mysqlSocket = new Socket(this.mysqlHost, this.mysqlPort);
+            this.mysqlIn = this.mysqlSocket.getInputStream();
+            this.mysqlOut = this.mysqlSocket.getOutputStream();
+            System.err.print("Connected to mysql host.\n");
         }
         catch (IOException e) {
             return;
+        }
+    }
+
+    public void run() {
+        while (this.running == 1) {
+                
+            this.readClient();
+            this.dumpBuffer();
+            this.writeMysql();
+                
+            this.readMysql();
+            this.dumpBuffer();
+            this.writeClient();
+        }
+        System.err.print("Exiting thread.\n");
+    }
+    
+    public void clearBuffer() {
+        this.buffer.clear();
+    }
+    
+    public void readClient() {
+        int b = 0;
+        try {
+            // Read from the client
+            while (this.clientIn.available() > 0) {
+                b = this.clientIn.read();
+                if (b == -1) {
+                    this.running = 0;
+                    return;
+                }
+                this.buffer.add(b);
+            }
+            if (this.buffer.size() > 0)
+                System.err.print("Reading from Client "+this.buffer.size()+" bytes.\n");
+        }
+        catch (IOException e) {
+            this.running = 0;
+        }
+    }
+
+    public void readMysql() {
+        int b = 0;
+        try {
+            // Read from the client
+            while (this.mysqlIn.available() > 0) {
+                b = this.mysqlIn.read();
+                if (b == -1) {
+                    this.running = 0;
+                    return;
+                }
+                this.buffer.add(b);
+            }
+            if (this.buffer.size() > 0)
+                System.err.print("Reading from MySQL "+this.buffer.size()+" bytes.\n");
+        }
+        catch (IOException e) {
+            this.running = 0;
+        }
+    }
+    
+    public void writeClient() {
+        int size = this.buffer.size();
+        int b = 0;
+        int i = 0;
+        
+        if (size == 0)
+            return;
+        
+        try {
+            System.err.print("Writing to client "+this.buffer.size()+" bytes.\n");
+            
+            for (i = 0; i < size; i++) {
+                b = this.buffer.get(i);
+                this.clientOut.write(b);
+            }
+            this.clearBuffer();
+        }
+        catch (IOException e) {
+            this.running = 0;
+        }
+    }
+
+    public void writeMysql() {
+        int size = this.buffer.size();
+        int b = 0;
+        int i = 0;
+        
+        if (size == 0)
+            return;
+        
+        try {
+            System.err.print("Writing to MySQL "+this.buffer.size()+" bytes.\n");
+            
+            for (i = 0; i < size; i++) {
+                b = this.buffer.get(i);
+                this.mysqlOut.write(b);
+            }
+            this.clearBuffer();
+        }
+        catch (IOException e) {
+            this.running = 0;
+        }
+    }
+    
+    public void dumpBuffer() {
+        int size = this.buffer.size();
+        Integer b = 0;
+        int i = 0;
+        byte[] buff = new byte[size];
+        
+        for (i = 0; i < size; i++) {
+            b = this.buffer.get(i);
+            buff[i] = (byte) (b & 0xFF);
+        }
+        
+        if (size > 0) {
+            try {
+                HexDump.dump(buff, 0, java.lang.System.err, 0);
+            }
+            catch (IOException e) {
+                return;
+            }
         }
     }
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x 
 
-javac *.java
-java -DmysqlHost=127.0.0.1 -DmysqlPort=3306 -Dport=5050 JMTC
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CLASSPATH=""
+CLASSPATH="${CLASSPATH}:lib/commons-io-2.4.jar"
+CLASSPATH="${CLASSPATH}:."
+
+set CLASSPATH="${CLASSPATH}"
+export CLASSPATH="${CLASSPATH}"
+
+javac -classpath "${CLASSPATH}" *.java
+java -classpath "${CLASSPATH}" -DmysqlHost=127.0.0.1 -DmysqlPort=3306 -Dport=5050 JMTC
