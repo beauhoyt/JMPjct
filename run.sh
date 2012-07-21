0956cd91cceaa5b83a827dd35438da023bf5d2ba
Rob Smith <kormoc@gmail.com>
2012-07-20 20:51:06 -0700

    Reorginize directory

--- run.sh
+++ run.sh
@@ -7,28 +7,45 @@ set -o pipefail
 JAVAOPT="-Xmx4096m"
 
 PROXYOPT=""
+CLASSPATH=""
 
 # Connection options
 PROXYOPT="${PROXYOPT} -DmysqlHost=127.0.0.1 -DmysqlPort=3306 -Dport=5050"
 
 # Logging Config File
-PROXYOPT="${PROXYOPT} -DlogConf=log.conf"
+PROXYOPT="${PROXYOPT} -DlogConf=conf/log.conf"
 
 # Plugins
 #PROXYOPT="${PROXYOPT} -Dplugins=Plugin_Debug,Plugin_Example"
-PROXYOPT="${PROXYOPT} -Dplugins=Plugin_Debug"
+#PROXYOPT="${PROXYOPT} -Dplugins=Plugin_Debug"
+PROXYOPT="${PROXYOPT} -Dplugins=Plugin_Debug,Plugin_Ehcache"
 
-# Load up the class path automagically
-CLASSPATH=""
-for file in `find lib -name *.jar`
+# Core components
+CLASSPATH="${CLASSPATH}:core"
+
+# Locate the plugins
+for path in `find plugins -type d`
 do
-    CLASSPATH="${CLASSPATH}${file}:"
+    CLASSPATH="${CLASSPATH}:${path}"
 done
 
-CLASSPATH="${CLASSPATH}."
+# Locate the conf files (Required for ehcache)
+CLASSPATH="${CLASSPATH}:conf"
+
+# Add any jar files
+for file in `find lib -name *.jar`
+do
+    CLASSPATH="${CLASSPATH}:${file}"
+done
+
+# Add cwd
+CLASSPATH="${CLASSPATH}:."
 
 set CLASSPATH="${CLASSPATH}"
 export CLASSPATH="${CLASSPATH}"
 
+# Compile
 javac -classpath "${CLASSPATH}" `find . -name \*.java | tr \\\n " "`
+
+# Run
 java ${JAVAOPT} -classpath "${CLASSPATH}" ${PROXYOPT} JMP
