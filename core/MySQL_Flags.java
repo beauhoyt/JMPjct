2bee16120da86647c6be3640ad02f1680ea85821
Rob Smith <kormoc@gmail.com>
2012-07-21 17:58:23 -0700

    Add in mysql column types

--- core/MySQL_Flags.java
+++ core/MySQL_Flags.java
@@ -80,4 +80,33 @@ interface MySQL_Flags {
     public static final int CLIENT_PS_MULTI_RESULTS             = 0x00040000;
     public static final int CLIENT_SSL_VERIFY_SERVER_CERT       = 0x40000000;
     public static final int CLIENT_REMEMBER_OPTIONS             = 0x80000000;
+    
+    public static final int MYSQL_TYPE_DECIMAL                  = 0x00;
+    public static final int MYSQL_TYPE_TINY                     = 0x01;
+    public static final int MYSQL_TYPE_SHORT                    = 0x02;
+    public static final int MYSQL_TYPE_LONG                     = 0x03;
+    public static final int MYSQL_TYPE_FLOAT                    = 0x04;
+    public static final int MYSQL_TYPE_DOUBLE                   = 0x05;
+    public static final int MYSQL_TYPE_NULL                     = 0x06;
+    public static final int MYSQL_TYPE_TIMESTAMP                = 0x07;
+    public static final int MYSQL_TYPE_LONGLONG                 = 0x08;
+    public static final int MYSQL_TYPE_INT24                    = 0x09;
+    public static final int MYSQL_TYPE_DATE                     = 0x0a;
+    public static final int MYSQL_TYPE_TIME                     = 0x0b;
+    public static final int MYSQL_TYPE_DATETIME                 = 0x0c;
+    public static final int MYSQL_TYPE_YEAR                     = 0x0d;
+    public static final int MYSQL_TYPE_NEWDATE                  = 0x0e;
+    public static final int MYSQL_TYPE_VARCHAR                  = 0x0f;
+    public static final int MYSQL_TYPE_BIT                      = 0x10;
+    public static final int MYSQL_TYPE_NEWDECIMAL               = 0xf6;
+    public static final int MYSQL_TYPE_ENUM                     = 0xf7;
+    public static final int MYSQL_TYPE_SET                      = 0xf8;
+    public static final int MYSQL_TYPE_TINY_BLOB                = 0xf9;
+    public static final int MYSQL_TYPE_MEDIUM_BLOB              = 0xfa;
+    public static final int MYSQL_TYPE_LONG_BLOB                = 0xfb;
+    public static final int MYSQL_TYPE_BLOB                     = 0xfc;
+    public static final int MYSQL_TYPE_VAR_STRING               = 0xfd;
+    public static final int MYSQL_TYPE_STRING                   = 0xfe;
+    public static final int MYSQL_TYPE_GEOMETRY                 = 0xff;
+
 }
